3527cb72edbb4acf67ad8c51f4046fe868449c05
Chad McCathie <chad@mccathie.com>
2018-03-14 15:35:00 +1100

    script to give slack a dark theme

---
+++ darkify_slack.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+cat << 'EOF' >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
+document.addEventListener('DOMContentLoaded', function() {
+ $.ajax({
+   url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
+   success: function(css) {
+     $("<style></style>").appendTo('head').html(css);
+   }
+ });
+});
+EOF
+echo If Slack.app is already running, refresh with CMD-R
+
+
